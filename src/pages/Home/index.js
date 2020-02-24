b84decd30a62d214c6254c3bdaea5838be626c24
lliuti <dev.lliuti@gmail.com>
2020-02-24 16:45:34 -0300

    adding Tips component to Main page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Intro from '../../components/Intro/index';
+import Tips from '../../components/Tips/index';
 
 export default function Home() {
   return (
     <>
       <Intro/>
+      <Tips/>
     </>
   );
 }
